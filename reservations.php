8a83d0884c0982807dd05e3dcc77adbfc875c060
carloscabani <cacabani@espol.edu.ec>
2024-08-24 20:45:19 -0500

    Modificaciones backend

--- reservations.php
+++ reservations.php
@@ -2,12 +2,48 @@
 session_start();
 include 'reservations_functions.php';
 
-if (!isset($_SESSION['userId'])) {
-    header('Location: login_register.php');
+//if (!isset($_SESSION['userId'])) {
+//    header('Location: login_register.php');
+//    exit();
+//}
+
+//$userId = $_SESSION['userId'];
+$userId =4;
+$_SESSION['username'] = 'kevin';
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['guideId'])) {
+    $guideId = $_GET['guideId'];
+    $reservations = getReservations($userId);
+
+    $response = null;
+
+    foreach ($reservations as $reservation) {
+        $details = explode(' reservado por Usuario ', $reservation);
+        $guideName = $details[0];
+        $reservedGuideId = $details[1];
+        
+        if ($reservedGuideId == $guideId) {
+            $response = [
+                'id' => $reservedGuideId,
+                'user' => $_SESSION['username'], 
+                'reservado' => true,
+                'guia' => $guideName
+            ];
+            break;
+        }
+    }
+
+    if ($response) {
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } else {
+        header('Content-Type: application/json', true, 404);
+        echo json_encode(['error' => 'Reserva no encontrada']);
+    }
     exit();
 }
 
-$userId = $_SESSION['userId'];
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['cancel'])) {
